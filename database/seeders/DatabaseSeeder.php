857d9605b58ffcf6b17b19ee92a422396b2f5223
Tokaa7 <ksoares@outlook.fr>
2023-12-20 23:53:25 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2493,8 +2493,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Rencontre::create([
-        'ref_equipe1' => DB::table('equipes')->where('nom', 'DRX')->value('id_equipe'),
-        'ref_equipe2' => DB::table('equipes')->where('nom', 'Gen.G')->value('id_equipe'),
+        'ref_equipe1' => DB::table('equipes')->where('nom', 'G2 Esports')->value('id_equipe'),
+        'ref_equipe2' => DB::table('equipes')->where('nom', 'Excel')->value('id_equipe'),
         'format' => '2', 
         'date' => '2023-12-21 17:00:00'
         ]);
